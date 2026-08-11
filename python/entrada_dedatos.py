cea4d96032ae914804d4321022f0d325d3170e12
lucas <lucas.lazalvia020@gmail.com>
2026-08-11 19:46:50 -0300

    Agrego análisis de ventas

--- python/entrada_dedatos.py
+++ python/entrada_dedatos.py
@@ -1,10 +1,28 @@
 contador = 1
 total = 0
+venta_mayor = 0
+venta_menor = 0
 
 while contador <= 5:
     print(f"Venta {contador}")
     gasto = int(input("¿Cuánto gastó el cliente? "))
+
     total = total + gasto
+
+    if contador == 1:
+        venta_mayor = gasto
+        venta_menor = gasto
+        
+    if gasto > venta_mayor:
+        venta_mayor = gasto
+    if gasto < venta_menor:
+        venta_menor = gasto
+
+
     contador = contador + 1
 
 print(f"Total vendido: ${total}")
+promedio = total / 5
+print(f'El promedio de ventas es de ${promedio}')
+print(f"La venta mas grande fue de $ {venta_mayor}")
+print(f"La venta mas chica fue de $ {venta_menor}")
